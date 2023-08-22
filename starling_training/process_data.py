a7a0fb13cef1a8f3580c85796847b2b8a962c504
Rishabh Bhardwaj <32847115+Bhardwaj-Rishabh@users.noreply.github.com>
2023-08-22 10:17:18 +0800

    Add files via upload

---
+++ starling_training/process_data.py
@@ -0,0 +1,9 @@
+import pickle as pk
+data1 = pk.load(open('./data/data_blue_finetune.pkl','rb'))
+data2 = pk.load(open('./data/data_helpful_finetune.pkl','rb'))
+data3 = pk.load(open('./data/data_sgpt_only.pkl','rb'))
+data_combined = data1+data2+data3
+
+random.shuffle(data_combined)
+with open("./data/data_combined.json", "w", encoding='utf-8') as f:
+    json.dump(data_combined, f, ensure_ascii=False, indent=4)
