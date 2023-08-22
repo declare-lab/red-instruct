95012af33eb1c4b72cb1edd60087547eaf83451d
Bhardwaj-Rishabh <rishabhbhardwaj15@gmail.com>
2023-08-22 10:37:42 +0800

    edit process_data.py and deepspeed configs from zero 2 to 1

--- starling_training/process_data.py
+++ starling_training/process_data.py
@@ -1,5 +1,7 @@
 import pickle as pk
 import random
+import json
+
 data1 = pk.load(open('./data/data_blue_finetune.pkl','rb'))
 data2 = pk.load(open('./data/data_helpful_finetune.pkl','rb'))
 data3 = pk.load(open('./data/data_sgpt_only.pkl','rb'))
@@ -8,3 +10,5 @@ data_combined = data1+data2+data3
 random.shuffle(data_combined)
 with open("./data/data_combined.json", "w", encoding='utf-8') as f:
     json.dump(data_combined, f, ensure_ascii=False, indent=4)
+
+print("Done...")
